5748c00cd0256b5a4615bf2c4eda6f10f8c0a966
Ayanami <yangtch@outlook.com>
2023-09-18 14:55:23 +0800

    新增坐标注意力模块
    Signed-off-by: Ayanami <yangtch@outlook.com>

--- basicsr/archs/rrdabnet_arch.py
+++ basicsr/archs/rrdabnet_arch.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn as nn
+import math
 from torch.nn import functional as F
 
 from basicsr.utils.registry import ARCH_REGISTRY
@@ -38,6 +39,60 @@ class ResidualDenseBlock(nn.Module):
         # Empirically, we use 0.2 to scale the residual for better performance
         return x5 * 0.2 + x
 
+class h_sigmoid(nn.Module):
+    def __int__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu=nn.ReLU6(inplace==inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=32):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        identity = x
+
+        n, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * a_w * a_h
+
+        return out
+
 
 class RRDB(nn.Module):
     """Residual in Residual Dense Block.
@@ -56,6 +111,8 @@ class RRDB(nn.Module):
         self.rdb3 = ResidualDenseBlock(num_feat, num_grow_ch, kernel_size=7)
         # 降维1x1卷积层
         self.reduce_dim = nn.Conv2d(num_feat * 3, num_feat, 1)
+        #CA 坐标注意力模块
+        self.CA = CoordAtt(num_feat, num_feat)
 
     def forward(self, x):
         out1 = self.rdb1(x)
@@ -68,6 +125,9 @@ class RRDB(nn.Module):
         # 通过1x1卷积层降维
         reduced_features = self.reduce_dim(concat_features)
 
+        # 融合坐标特征
+        ca_features = self.CA(reduced_features)
+
         # Empirically, we use 0.2 to scale the residual for better performance
-        return reduced_features * 0.2 + x
+        return reduced_features * 0.2 + ca_features * 0.2 + x
 

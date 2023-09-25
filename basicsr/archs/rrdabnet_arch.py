5fc02912a51210a5a94546c335f47161e52e2e36
Ayanami <yangtch@outlook.com>
2023-09-25 14:44:07 +0800

    修改数据集路径
    Signed-off-by: Ayanami <yangtch@outlook.com>

--- basicsr/archs/rrdabnet_arch.py
+++ basicsr/archs/rrdabnet_arch.py
@@ -1,13 +1,11 @@
 import torch
 from torch import nn as nn
-import math
 from torch.nn import functional as F
-
 from basicsr.utils.registry import ARCH_REGISTRY
 from .arch_util import default_init_weights, make_layer, pixel_unshuffle
 
 
-class ResidualDenseBlock(nn.Module):
+class ResidualDenseBlock_3(nn.Module):
     """Residual Dense Block.
 
     Used in RRDB block in ESRGAN.
@@ -17,13 +15,13 @@ class ResidualDenseBlock(nn.Module):
         num_grow_ch (int): Channels for each growth.
     """
 
-    def __init__(self, num_feat=64, num_grow_ch=32, kernel_size=3):
-        super(ResidualDenseBlock, self).__init__()
-        self.conv1 = nn.Conv2d(num_feat, num_grow_ch, kernel_size, 1, 1)
-        self.conv2 = nn.Conv2d(num_feat + num_grow_ch, num_grow_ch, kernel_size, 1, 1)
-        self.conv3 = nn.Conv2d(num_feat + 2 * num_grow_ch, num_grow_ch, kernel_size, 1, 1)
-        self.conv4 = nn.Conv2d(num_feat + 3 * num_grow_ch, num_grow_ch, kernel_size, 1, 1)
-        self.conv5 = nn.Conv2d(num_feat + 4 * num_grow_ch, num_feat, kernel_size, 1, 1)
+    def __init__(self, num_feat=64, num_grow_ch=32, ):
+        super(ResidualDenseBlock_3, self).__init__()
+        self.conv1 = nn.Conv2d(num_feat, num_grow_ch, 3, 1, 1)
+        self.conv2 = nn.Conv2d(num_feat + num_grow_ch, num_grow_ch, 3, 1, 1)
+        self.conv3 = nn.Conv2d(num_feat + 2 * num_grow_ch, num_grow_ch, 3, 1, 1)
+        self.conv4 = nn.Conv2d(num_feat + 3 * num_grow_ch, num_grow_ch, 3, 1, 1)
+        self.conv5 = nn.Conv2d(num_feat + 4 * num_grow_ch, num_feat, 3, 1, 1)
 
         self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
 
@@ -32,6 +30,73 @@ class ResidualDenseBlock(nn.Module):
 
     def forward(self, x):
         x1 = self.lrelu(self.conv1(x))
+        import ipdb;ipdb.set_trace()
+        x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
+        x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
+        x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
+        x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
+        # Empirically, we use 0.2 to scale the residual for better performance
+        return x5 * 0.2 + x
+
+class ResidualDenseBlock_5(nn.Module):
+    """Residual Dense Block.
+
+    Used in RRDB block in ESRGAN.
+
+    Args:
+        num_feat (int): Channel number of intermediate features.
+        num_grow_ch (int): Channels for each growth.
+    """
+
+    def __init__(self, num_feat=64, num_grow_ch=32, ):
+        super(ResidualDenseBlock_5, self).__init__()
+        self.conv1 = nn.Conv2d(num_feat, num_grow_ch, 5, 1, 1)
+        self.conv2 = nn.Conv2d(num_feat + num_grow_ch, num_grow_ch, 5, 1, 1)
+        self.conv3 = nn.Conv2d(num_feat + 2 * num_grow_ch, num_grow_ch, 5, 1, 1)
+        self.conv4 = nn.Conv2d(num_feat + 3 * num_grow_ch, num_grow_ch, 5, 1, 1)
+        self.conv5 = nn.Conv2d(num_feat + 4 * num_grow_ch, num_feat, 5, 1, 1)
+
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+        # initialization
+        default_init_weights([self.conv1, self.conv2, self.conv3, self.conv4, self.conv5], 0.1)
+
+    def forward(self, x):
+        x1 = self.lrelu(self.conv1(x))
+        import ipdb;ipdb.set_trace()
+        x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
+        x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
+        x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
+        x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
+        # Empirically, we use 0.2 to scale the residual for better performance
+        return x5 * 0.2 + x
+
+class ResidualDenseBlock_7(nn.Module):
+    """Residual Dense Block.
+
+    Used in RRDB block in ESRGAN.
+
+    Args:
+        num_feat (int): Channel number of intermediate features.
+        num_grow_ch (int): Channels for each growth.
+    """
+
+    def __init__(self, num_feat=64, num_grow_ch=32, ):
+        super(ResidualDenseBlock_7, self).__init__()
+        self.conv1 = nn.Conv2d(num_feat, num_grow_ch, 7, 1, 1)
+        self.conv2 = nn.Conv2d(num_feat + num_grow_ch, num_grow_ch, 7, 1, 1)
+        self.conv3 = nn.Conv2d(num_feat + 2 * num_grow_ch, num_grow_ch, 7, 1, 1)
+        self.conv4 = nn.Conv2d(num_feat + 3 * num_grow_ch, num_grow_ch, 7, 1, 1)
+        self.conv5 = nn.Conv2d(num_feat + 4 * num_grow_ch, num_feat, 7, 1, 1)
+
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+        # initialization
+        default_init_weights([self.conv1, self.conv2, self.conv3, self.conv4, self.conv5], 0.1)
+
+    def forward(self, x):
+        x1 = self.lrelu(self.conv1(x))
+        import ipdb;ipdb.set_trace()
         x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
         x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
         x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
@@ -40,12 +105,13 @@ class ResidualDenseBlock(nn.Module):
         return x5 * 0.2 + x
 
 class h_sigmoid(nn.Module):
-    def __int__(self, inplace=True):
+    def __init__(self, inplace=True):
         super(h_sigmoid, self).__init__()
-        self.relu=nn.ReLU6(inplace==inplace)
+        self.relu = nn.ReLU6(inplace=inplace)
 
     def forward(self, x):
         return self.relu(x + 3) / 6
+
 
 class h_swish(nn.Module):
     def __init__(self, inplace=True):
@@ -93,8 +159,7 @@ class CoordAtt(nn.Module):
 
         return out
 
-
-class RRDB(nn.Module):
+class RRDAB(nn.Module):
     """Residual in Residual Dense Block.
 
     Used in RRDB-Net in ESRGAN.
@@ -105,17 +170,19 @@ class RRDB(nn.Module):
     """
 
     def __init__(self, num_feat, num_grow_ch=32):
-        super(RRDB, self).__init__()
-        self.rdb1 = ResidualDenseBlock(num_feat, num_grow_ch, kernel_size=3)
-        self.rdb2 = ResidualDenseBlock(num_feat, num_grow_ch, kernel_size=5)
-        self.rdb3 = ResidualDenseBlock(num_feat, num_grow_ch, kernel_size=7)
+        super(RRDAB, self).__init__()
+        self.rdb1 = ResidualDenseBlock_3(num_feat, num_grow_ch)
+
+        self.rdb2 = ResidualDenseBlock_5(num_feat, num_grow_ch)
+        self.rdb3 = ResidualDenseBlock_7(num_feat, num_grow_ch)
         # 降维1x1卷积层
         self.reduce_dim = nn.Conv2d(num_feat * 3, num_feat, 1)
-        #CA 坐标注意力模块
+        # CA 坐标注意力模块
         self.CA = CoordAtt(num_feat, num_feat)
 
     def forward(self, x):
         out1 = self.rdb1(x)
+        import ipdb;ipdb.set_trace()
         out2 = self.rdb2(x)
         out3 = self.rdb3(x)
 
@@ -131,3 +198,39 @@ class RRDB(nn.Module):
         # Empirically, we use 0.2 to scale the residual for better performance
         return reduced_features * 0.2 + ca_features * 0.2 + x
 
+
+@ARCH_REGISTRY.register()
+class RRDABNet(nn.Module):
+    def __init__(self, num_in_ch, num_out_ch, scale=4, num_feat=64, num_block=23, num_grow_ch=32):
+        super(RRDABNet, self).__init__()
+        self.scale = scale
+        if scale == 2:
+            num_in_ch = num_in_ch * 4
+        elif scale == 1:
+            num_in_ch = num_in_ch * 16
+        self.conv_first = nn.Conv2d(num_in_ch, num_feat, 3, 1, 1)
+        self.body = make_layer(RRDAB, num_block, num_feat=num_feat, num_grow_ch=num_grow_ch)
+        self.conv_body = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        # upsample
+        self.conv_up1 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        self.conv_up2 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        self.conv_hr = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+        self.conv_last = nn.Conv2d(num_feat, num_out_ch, 3, 1, 1)
+
+        self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+
+    def forward(self, x):
+        if self.scale == 2:
+            feat = pixel_unshuffle(x, scale=2)
+        elif self.scale == 1:
+            feat = pixel_unshuffle(x, scale=4)
+        else:
+            feat = x
+        feat = self.conv_first(feat)
+        body_feat = self.conv_body(self.body(feat))
+        feat = feat + body_feat
+        # upsample
+        feat = self.lrelu(self.conv_up1(F.interpolate(feat, scale_factor=2, mode='nearest')))
+        feat = self.lrelu(self.conv_up2(F.interpolate(feat, scale_factor=2, mode='nearest')))
+        out = self.conv_last(self.lrelu(self.conv_hr(feat)))
+        return out
--- basicsr/archs/rrdbnet_arch.py
+++ basicsr/archs/rrdbnet_arch.py
@@ -31,7 +31,9 @@ class ResidualDenseBlock(nn.Module):
 
     def forward(self, x):
         x1 = self.lrelu(self.conv1(x))
+        # import ipdb;ipdb.set_trace()
         x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
+
         x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
         x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
         x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
